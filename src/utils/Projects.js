684063df23bec7dc6c4362a4fe572c8101f357a7
ericggul <57379903+ericggul@users.noreply.github.com>
2022-08-01 13:50:27 +0900

    Feat

--- src/utils/Projects.js
+++ src/utils/Projects.js
@@ -1,56 +1,56 @@
 export const P0 = {
-    name: 'Identity',
-    type: 'P0',
-    assign: 'Self Introduction',
-    introduction: `An unique style of revealing myself, with an influence from Piet Mondrian. Three symbolic icons are used to represent myself: Black Swan, Statue of Liberty, and Andy Warhol's Campbell's Soup Cans. Click on each rectangle to reveal myself.`,
-    link: 'https://ericggul.github.io/DTC/Assignment%201/P0%20Revised%202/index.html'
-}
+  name: "Identity",
+  type: "P0",
+  assign: "Self Introduction",
+  introduction: `An unique style of revealing myself, with an influence from Piet Mondrian. Three symbolic icons are used to represent myself: Black Swan, Statue of Liberty, and Andy Warhol's Campbell's Soup Cans. Click on each rectangle to reveal myself.`,
+  link: "https://ericggul.github.io/DTC/Assignment%201/P0%20Revised%202/index.html",
+};
 
 export const P1 = {
-    name: 'Born, Live and Die',
-    type: 'P1',
-    assign: 'Small Multiples',
-    introduction: `A Geometrical Axiom, such as "Orthocenter, Circumcenter, Gravity Points share collinearity.", are derived from our Accumulated Abstract Knowledge.
+  name: "Born, Live and Die",
+  type: "P1",
+  assign: "Small Multiples",
+  introduction: `A Geometrical Axiom, such as "Orthocenter, Circumcenter, Gravity Points share collinearity.", are derived from our Accumulated Abstract Knowledge.
     However, in order for us to understand the geometrical axiom, we should always interpret the abstract knowledge into the descriptive geometrical shapes.
     The Euclidean lines and circles above offers us a bridge that links to the abstract world, but still it takes part within the descriptive images.
     My purpose of this project was to create the new usage of these Euclidean images, by highlighting its descriptiveness rather than its abstractness.
     Can Euclidean lines and circles themselves create an aesthetic impression?`,
-    link: 'https://ericggul.github.io/DTC/P1/'
-}
+  link: "https://ericggul.github.io/DTC/P1/",
+};
 
 export const P2 = {
-    name: 'Liberty St. & Ford Ave.',
-    type: 'P2',
-    assign: 'Infographics',
-    introduction: `This infographics criticizes Korea's Totalitarian New Towns(신도시).
+  name: "Liberty St. & Ford Ave.",
+  type: "P2",
+  assign: "Infographics",
+  introduction: `This infographics criticizes Korea's Totalitarian New Towns(신도시).
     New Towns are like 'Liberty Street and Ford Avenue': A random street and random avenue, filled with identical apartments, which can be find anywhere but cannot be distinguished.
     However, their real situation are positioning the opposite direction, which can be summarized as 'Anti Liberty and Distorted Fordism': Anti-Liberty in terms of its lifestyle, and Distorted Fordism in terms of its failure of delivering cheap mass-produced residential flats.
     This infographics highlights this paradox in six dimensions, questioning and criticizing the lifestyle of many Koreans.`,
-    link: `https://ericggul.github.io/plz/#/P2`
-}
+  link: `https://ericggul.github.io/Tril.ogy/#/P2`,
+};
 
 export const P3 = {
-    name: 'Geur Van Eeen Tulip (Scent of a Tulip)',
-    type: 'P3',
-    assign: 'Data Visualization',
-    introduction: `"History teaches us that man learns nothing from history", Hegel had quoted. 
+  name: "Geur Van Eeen Tulip (Scent of a Tulip)",
+  type: "P3",
+  assign: "Data Visualization",
+  introduction: `"History teaches us that man learns nothing from history", Hegel had quoted. 
     In recent years, stock price of IT companies, notably FAANG and Microsoft, had risen tremendously, reminding the historical bubble of dutch tulip.
     This Data Visualization shows the recent 5 years stock price distribution of FAANG and Microsoft, highlighting collective madness underlying the current IT stock market.
     The presentation of stock price is architected based on experimential and regressive approach, rather than the inductive approach: 
     Experiemntial and regressive approach states that the stock price will soar down, as currently the price is overwhelmed. The inductive approach, on the other hand, claims that stock price will keep to rise, as it had for the past 5 years.
     Traditional Time-Series graph with time on x-axis and price on y-axis had caused people to think indutively. Contrastly, this visualization displaying the stock price distribution assists people to view the current situation from the regressive perspective.`,
-    link: 'https://ericggul.github.io/plz/#/P3'
-}
+  link: "https://ericggul.github.io/Tril.ogy/#/P3",
+};
 
 export const P35 = {
-    name: 'In Coin We Trust',
-    type: 'P3.5',
-    assign: 'Data Visualization(Beta)',
-    introduction: `This data visualization uses the same regressive approach used at the P3, displaying the Bitcoin price distribution for past 5 years.
+  name: "In Coin We Trust",
+  type: "P3.5",
+  assign: "Data Visualization(Beta)",
+  introduction: `This data visualization uses the same regressive approach used at the P3, displaying the Bitcoin price distribution for past 5 years.
     However, the tone of narratives behind the presentation is somehow different.
     For the FAANG companies in P3, it was possible to present constructive criticism arguing that current stock market is overwhelmed.
     Here for the Bitcoin, no criticism is allowed: The range of bubble had already gone beyond the common sense. 
     I had no choice but to leave a cynical ridicule to this blindly religious status quo.
     `,
-    link: 'https://ericggul.github.io/plz/#/P35'
-}
+  link: "https://ericggul.github.io/Tril.ogy/#/P35",
+};
